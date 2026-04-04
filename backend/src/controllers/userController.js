121bb2054ccbad7a3172388826bd75cb370238fe
Luiz <luikznn364@gmail.com>
2026-04-04 14:18:35 -0300

    add hash de senhas

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,14 +1,17 @@
 import db from '../config/db.js'
+import bcrypt from 'bcrypt'
 
 export const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
 
     try {
+        const hash = await bcrypt.hash(senha, 10)
+
         const sql = "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)"
-        const [result] = await db.execute(sql, [nome, email, senha])
-        res.status(201).json({ message: "Usuário criado!", id: result.insertId})
+        const [result] = await db.execute(sql, [nome, email, hash])
+        res.status(201).json({ message: "Usuário criado!", id: result.insertId })
     }
     catch (error) {
-        res.status(500).json({ error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
